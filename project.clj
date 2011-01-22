f9ccc9bc910ebd361ec3123e11ccf5c147a5b99f
Eric Lavigne <lavigne.eric@gmail.com>
2011-01-22 06:45:30 -0500

    No longer depending on specific version of clojure or clojure-contrib.

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject leiningen-run "0.3"
   :description "Leiningen plugin that adds a run task"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-		 [org.clojure/clojure-contrib "1.1.0"]
-		 [org.apache.maven/maven-ant-tasks "2.0.10"]]
+  :dependencies [[org.apache.maven/maven-ant-tasks "2.0.10"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
 
